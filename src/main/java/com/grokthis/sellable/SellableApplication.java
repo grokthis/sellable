2357664ba14f9f99f6d6cb3d5615ee651c8a9e51
Robert Butler <robert@grokthiscommunity.net>
2022-07-18 23:16:27 -0700

    Initialize spring boot, add database

---
+++ src/main/java/com/grokthis/sellable/SellableApplication.java
@@ -0,0 +1,13 @@
+package com.grokthis.sellable;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SellableApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SellableApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/grokthis/sellable/SellableApplicationTests.java
@@ -0,0 +1,13 @@
+package com.grokthis.sellable;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SellableApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
