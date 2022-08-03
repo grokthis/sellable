a26ed5c4819874153759aae30ee0c0d2484c96fd
Robert Butler <robert@grokthiscommunity.net>
2022-08-02 20:13:36 -0700

    Optimize imports

--- src/main/java/com/grokthis/sellable/SellableApplication.java
+++ src/main/java/com/grokthis/sellable/SellableApplication.java
@@ -2,17 +2,6 @@ package com.grokthis.sellable;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.reactive.PathRequest;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.config.annotation.web.builders.WebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.web.server.ServerHttpSecurity;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.security.web.server.SecurityWebFilterChain;
-
-import javax.servlet.Filter;
-import javax.servlet.http.HttpServletRequest;
-import java.util.List;
 
 @SpringBootApplication
 public class SellableApplication {
